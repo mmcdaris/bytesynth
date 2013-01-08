2e79a4204dde2eaed1dfa1f2992c78b2c76434b1
Mo <morganmcdaris@yahoo.com>
2013-01-08 02:36:40 +0000

    first commit from ec2 :)

---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,4 @@
+Airbrake.configure do |config|
+  config.api_key = 'df70b6dc2acc78016a9a07f93e54403f'
+  config.host = 'collect.airbrake.io'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bytesynth::Application.config.secret_token = 'c49a6b48d47445263fd9ddd7e9ef411373647cb512a0049f9c4631f6ea859adf026b544e1ceedb8a0061e345a9136763b472bf6e24dc86c0f1e2af4c1f804a46'
