3cb237f2aa3dfab94c4a4bc21049b76601f71a8d
Morgan McDaris <morganmcdaris@yahoo.com>
2013-01-10 18:22:30 -0800

    load assets during deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ ssh_options[:forward_agent] = true
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
+load 'deploy/assets'
+
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
